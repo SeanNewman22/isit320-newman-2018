61d9cb7c98644a9831dfe575b581ad33fbccd06f
Sean Newman <sean.newman@bellevuecollege.edu>
2018-10-10 23:39:34 +0000

    built AWS client and server in class

---
+++ aws-provision/client/src/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            file: 'File name will be placed here.',
+            status: 'waiting for server'
+        };
+    }
+
+    queryServer = () => {
+        const that = this;
+        fetch('/foo')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                console.log('parsed json', json);
+                that.setState(foo => (json));
+            })
+            .catch(function(ex) {
+                console.log('parsing failed, URL bad, network down, or similar', ex);
+            });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                
+
+                <p className="App-intro">
+                    state: {this.state.status} file: {this.state.file}
+                </p>
+                <button onClick={this.queryServer}>Bar</button>
+            </div>
+        );
+    }
+}
+
+export default App;
