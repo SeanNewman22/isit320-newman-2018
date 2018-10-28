62aa41455032cbdd79a9272e7254b865c32cebd6
Ubuntu <ubuntu@ip-172-31-95-191.ec2.internal>
2018-10-28 14:21:00 +0000

    finished system check homework for EC2

---
+++ week06-system-check/server/public/precache-manifest.d673f8bdc9c481c6563197c057d131d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "900656386f1bdf739e58",
+    "url": "/static/js/main.90065638.chunk.js"
+  },
+  {
+    "revision": "7e417a777a4d86431b2a",
+    "url": "/static/js/1.7e417a77.chunk.js"
+  },
+  {
+    "revision": "900656386f1bdf739e58",
+    "url": "/static/css/main.79745ec8.chunk.css"
+  },
+  {
+    "revision": "a43e4c37048de0dd06a2fd3f6d1e2ef4",
+    "url": "/index.html"
+  }
+];
