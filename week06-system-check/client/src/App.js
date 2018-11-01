a94c65d88469046fe92a900bc1d0e9bc13354fd5
sean at school <sean.m.newman@comcast.net>
2018-10-31 17:15:26 -0700

    added code to system-check for mid term

--- week06-system-check/client/src/App.js
+++ week06-system-check/client/src/App.js
@@ -27,15 +27,59 @@ class App extends Component {
     };
 
     render() {
+
+        const radioWeb = (
+            <div className="container">
+                <form onSubmit={this.handleSubmit}>
+                    <fieldset>
+                        <div className="elf-form-field">
+
+                            <legend>Services</legend>
+                            <input
+                                type="radio"
+                                name="app-choice"
+                                data-endpoint="0"
+                                value="CpuInfo"
+                                id="elf-radio-cpu"
+                                onChange={this.handleChange}
+                            />
+                            <label htmlFor="elf-radio-cpu">CpuInfo</label>
+
+                            <input
+                                type="radio"
+                                name="app-choice"
+                                data-endpoint="0"
+                                value="VersionCheck"
+                                id="elf-radio-version"
+                                onChange={this.handleChange}
+                            />
+                            <label htmlFor="elf-radio-version">Version Info</label>
+
+                        </div>
+
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">Run System Script</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        );
+
         return (
             <div className="App">
                 <header>
                     <h1>Copy File</h1>
                 </header>
                 <main>
-                    <button onClick={this.copyFile}>Copy File</button>
-                    <pre>{this.state.allData}</pre>
+                    <section>
+                        {radioWeb}
+                    </section>
+                    <section>
+                        <pre>{this.state.allData}</pre>
+                    </section>
+                    <button onClick={this.runFoo}>Run Foo</button>
                 </main>
+
                 <footer>
                     <p>&copy; by Sean Newman</p>
                 </footer>
