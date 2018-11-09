50040ad6641df651ee859c34ceb13ec3caa93b93
Sean Newman <sean.newman@bellevuecollege.edu>
2018-11-09 04:46:29 +0000

    worked on midterm from cloud9

---
+++ midterm/server/routes/script-pusher.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+const spawn = require('child_process').spawn;
+
+let allData = '';
+
+const copyFile = () => {
+    return new Promise(function(resolve, reject) {
+        console.log('Run CPU info', process.env.SETUP_LINUXBOX);
+
+        const pushScript = spawn(process.env.SETUP_LINUXBOX + '/CpuInfo');
+
+        pushScript.stdout.on('data', data => {
+            console.log(`child stdout:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.log('PUSH', data);
+        });
+
+        pushScript.stderr.on('data', data => {
+            console.log(`child stderr:\n${data}`);
+            allData += 'PUSH-SCRIPT: ' + data;
+            //console.error('PUSH', data);
+        });
+
+        pushScript.on('close', code => {
+            resolve({
+                result: 'success',
+                allData: allData,
+                code: code
+            });
+        });
+
+        pushScript.on('error', code => {
+            reject({
+                result: 'error',
+                code: code
+            });
+        });
+    });
+};
+
+const check = (request, response, next) => {
+    console.log('REQUEST CHECK CALLED', request.query);
+    const validOptions = ['CpuInfo', 'VersionCheck', 'uptime'];
+    if (request.query.script) {
+        console.log('INSIDE REQUEST SCRIPT');
+        if (!validOptions.includes(request.query.script)) {
+            console.log('INSIDE REQUEST INVALID OPTION');
+            response.send({result: 'error', error: 'Invalid Option: ' + request.query.script, script: request.query.script});
+            return;
+        }
+    }
+    next();
+};
+
+router.use(check);
+
+router.get('/copy-file', (request, response) => {
+    //const result = { result: 'success'};
+    //response.send(result);
+
+    copyFile()
+        .then(result => {
+            console.log(JSON.stringify(result, null, 4));
+            response.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            response.send(err);
+        });
+});
+
+module.exports = router;
