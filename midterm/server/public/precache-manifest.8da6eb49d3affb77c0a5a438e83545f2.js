472dffb3392855f1f7a8d8027ef096f54d7ce8f8
sean at school <sean.m.newman@comcast.net>
2018-11-14 17:03:50 -0800

    added midterm

---
+++ midterm/server/public/precache-manifest.8da6eb49d3affb77c0a5a438e83545f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+    {
+        revision: '229c360febb4351a89df',
+        url: '/static/js/runtime~main.229c360f.js'
+    },
+    {
+        revision: 'b6f79a6b790d55ff5d35',
+        url: '/static/js/main.b6f79a6b.chunk.js'
+    },
+    {
+        revision: '85865cead4f7a7bdd0e9',
+        url: '/static/js/1.85865cea.chunk.js'
+    },
+    {
+        revision: 'b6f79a6b790d55ff5d35',
+        url: '/static/css/main.e4c64822.chunk.css'
+    },
+    {
+        revision: 'c771159d85eec74c1d8afb767a5c652c',
+        url: '/index.html'
+    }
+];
