34d1aa162da6d939d5d197caedc84c77e5da883f
Sean Newman <sean.newman@bellevuecollege.edu>
2018-11-19 07:42:57 +0000

    no longer hard coding IP and Private Key for System-check-refactor-week09 on branch week09 with tag v1.2.15.

--- week09-system-check-refactor/server/routes/ssh-runner.js
+++ week09-system-check-refactor/server/routes/ssh-runner.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Client = require('ssh2').Client;
+const elfUtils = require('elven-code').elfUtils;
 
 const hostAddress = '35.174.101.27';
 
@@ -42,8 +43,42 @@ const runUptime = (hostAddress, response) => {
     });
 };
 
-router.get('/run-uptime', (request, response) => {
-    runUptime(hostAddress, response);
+const getSshIp = () => {
+    return new Promise(function (resolve, reject) {
+        elfUtils.readFile(process.env.HOME + '/.ssh/config')
+            .then((content) => {
+                //var pattern = new RegExp('Host ec2-bc[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)[\\s\\S]\\s*(.*)');
+                var pattern = new RegExp('Host ec2-bc\n\t(.*)\n\t(.*)\n\t(.*)\n\t(.*)');
+                const result = {};
+                const match = content.result.match(pattern);
+                for (let i = 1; i < 5; i++) {
+                    if (match[i].startsWith('HostName')) {
+                        var hostPattern = new RegExp('HostName\\s(.*)');
+                        result.hostName = match[i].match(hostPattern)[1];
+                    }
+                    if (match[i].startsWith('IdentityFile')) {
+                        const idPattern = new RegExp('IdentityFile\\s(.*)');
+                        const path = match[i].match(idPattern)[1];
+                        result.identityFile = path.substring(path.lastIndexOf('/') + 1, path.length)
+                    }
+                }
+                resolve(result);
+            })
+            .catch(reject);
+    });
+};
+
+
+router.get('/run-uptime', function(request, response) {
+    console.log('run-get-started called in ssh-runner', hostAddress);
+    getSshIp()
+        .then((result) => {
+            runUptime(result.hostName, result.identityFile, response);
+        })
+        .catch((err) => {
+            response.send(err);
+        });
+
 });
 
 module.exports = router;
