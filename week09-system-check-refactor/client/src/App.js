89b5f5947da96f1d24f5bec2fa93ae86375c155e
Sean Newman <sean.newman@bellevuecollege.edu>
2018-11-19 07:19:28 +0000

    finished system-refactor for system-refactor on branch week09 with tag v1.2.14.

--- week09-system-check-refactor/client/src/App.js
+++ week09-system-check-refactor/client/src/App.js
@@ -7,12 +7,6 @@ import RadioRemote from './RadioRemote';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.dataEndPoints = ['/script-pusher/run-script?script=', '/script-pusher/run-system-tool?script='];
-        this.state = {
-            allData: '',
-            selectedValue: '',
-            endPointIndex: 0
-        };
     }
     
     render() {
