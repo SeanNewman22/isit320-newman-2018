e399e9ca6cd2e22240aeaf2080e9b2936fc2445e
sean at school <sean.m.newman@comcast.net>
2018-12-03 15:43:02 -0800

    making copy file just work

---
+++ week05-ec2-copy-file/server/public/precache-manifest.5cbf14c1eb731af5b5781ac7f877eae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af7df39b193087d421eb",
+    "url": "/static/js/main.af7df39b.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "af7df39b193087d421eb",
+    "url": "/static/css/main.edb5b407.chunk.css"
+  },
+  {
+    "revision": "415dc7c156f7a6a411fa2edbf74fa172",
+    "url": "/index.html"
+  }
+];
