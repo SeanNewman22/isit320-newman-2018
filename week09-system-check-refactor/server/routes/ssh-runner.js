9cd9a35b72cba087889b978ed233b5a22b847bac
sean at school <sean.m.newman@comcast.net>
2018-12-05 15:57:22 -0800

    fixing uptime call on ssh-runner

--- week09-system-check-refactor/server/routes/ssh-runner.js
+++ week09-system-check-refactor/server/routes/ssh-runner.js
@@ -11,7 +11,7 @@ const runUptime = (host, idFile, response) => {
     var conn = new Client();
     conn.on('ready', function () {
         console.log('Client :: ready');
-        conn.exec('~/uptime', function (err, stream) {
+        conn.exec('/usr/bin/uptime', function (err, stream) {
             if (err) throw err;
             stream
                 .on('close', function (code, signal) {
