0e21f4febb43e0b567ea3da51245a61dd66614f4
sean at school <sean.m.newman@comcast.net>
2018-12-05 16:20:48 -0800

    fixing midterm

---
+++ week09-system-check-refactor/client/build/precache-manifest.5ff1706cb2c6c76f7143808f034300b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96d87d22944d5c25605d",
+    "url": "/static/js/main.96d87d22.chunk.js"
+  },
+  {
+    "revision": "8a837f4a82d0c9979ede",
+    "url": "/static/js/1.8a837f4a.chunk.js"
+  },
+  {
+    "revision": "96d87d22944d5c25605d",
+    "url": "/static/css/main.6a1b0e1f.chunk.css"
+  },
+  {
+    "revision": "64391cdad9a447f5e48383c7a4c30d9e",
+    "url": "/index.html"
+  }
+];
